36b175843e2cfba268ab1c49d6839dbecda80afd
bubsworth <bubbard96@hotmail.com>
2024-11-29 16:12:34 +0000

    4. Working with Object Types

---
+++ first-app.js
@@ -0,0 +1,6 @@
+var userName = 'Max';
+// userName = 'Max';
+userName = 'Max';
+var userAge = 34;
+var isValid = true;
+//string, number, boolean
--- first-app.ts
+++ first-app.ts
@@ -1,1 +1,34 @@
 let userName = 'Max';
+
+// userName = 'Max';
+userName = 'Max';
+
+let userAge = 34;
+let isValid = true;
+
+//string, number, boolean
+
+let userID: string | number = 'abc1'
+userID = 123; 
+
+// userID = true;
+
+// let user: object;
+
+let user: {
+  name: string;
+  age: number;
+  isAdmin: boolean;
+  id: string; //123 | 'abc' | true | false | null | undefined
+};
+
+//user = 'Max';
+
+user = {
+  name: 'Max',
+  age: 34,
+  isAdmin: true,
+  id: 'abc' //123
+}
+
+// user = {};
