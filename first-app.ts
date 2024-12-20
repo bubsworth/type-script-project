6f2456ecbd4ad76a22feefc7ed8d795d4ca3df88
bubsworth <bubbard96@hotmail.com>
2024-12-20 14:01:24 +0000

    21. Merging Types

--- first-app.ts
+++ first-app.ts
@@ -94,3 +94,39 @@ class AuthCredentials implements Credentials {
 function login(credentials: Credentials) {}
 
 login(new AuthCredentials());
+
+//*** Merging Types **///
+
+// type Admin = {
+//   permissions: string[];
+// };
+
+// type AppUser = {
+//   userName: string;
+// };
+
+// type AppAdmin = Admin & AppUser;
+
+// let admin: AppAdmin;
+
+// admin = {
+//   permissions: ["login"],
+//   userName: "Max",
+// };
+
+interface Admin {
+  permissions: string[];
+}
+
+interface AppUser {
+  userName: string;
+}
+
+interface AppAdmin extends Admin, AppUser {}
+
+let admin: AppAdmin;
+
+admin = {
+  permissions: ["login"],
+  userName: "Max",
+};
